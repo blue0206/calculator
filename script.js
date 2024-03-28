8fb8ad20f7adf543d49918b40e5ef0bc33cfb030
Aayush Rai <coolaayushrai@gmail.com>
2024-03-28 23:36:08 +0530

    Add click event listeners to buttons to show on screen
    Since the calculator screen must show the characters pressed on screen,
    added click event listeners to all buttons to display them on screen
    upon click.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const screen = document.querySelector('.screen');
+screen.textContent = '';
+
+const btnList = Array.from(document.querySelectorAll('.btn'));
+btnList.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        screen.textContent += e.target.id;
+    })
+});
+
 function add(num1, num2)
 {
     return (num1 + num2);
