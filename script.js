184560e561bb09e238edf8d219b8bc1d8f76e612
Aayush Rai <coolaayushrai@gmail.com>
2024-03-28 23:49:04 +0530

    Add backspace button, create respective event listeners for AC & backspace
    The 'C' button has been replaced by 'backspace' button because essentially
    'AC' and 'C' were doing the same thing, hence being redundant.
    Backspace button will help clear one-character mistakes unlike the 'AC'
    which resets everything.
    
    Clicking the 'AC' button now clears the entire screen while clicking the
    backspace button clears one character at a time.

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ screen.textContent = '';
 
 const btnList = Array.from(document.querySelectorAll('.btn'));
 btnList.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-        screen.textContent += e.target.id;
-    })
+    if (btn.id != 'ac' && btn.id != 'backspace' && btn.id != '=')
+    {
+        btn.addEventListener('click', (e) => {
+            screen.textContent += e.target.id;
+        });
+    }
 });
+
+document.querySelector('#ac').addEventListener('click', () => {
+    screen.textContent = '';
+});
+
+document.querySelector('#backspace').addEventListener('click', () => {
+    const arr = screen.textContent.split('');
+    arr.pop();
+    screen.textContent = arr.join('');    
+})
 
 function add(num1, num2)
 {
